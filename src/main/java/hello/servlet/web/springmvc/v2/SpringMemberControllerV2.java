7b835493521b5b4cfeffb5ff90cfbd4697055fb3
박찬석 <chanseok2323@gmail.com>
2021-10-24 00:00:50 +0900

    study-spring-mvc 수정

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -3,7 +3,10 @@ package hello.servlet.web.springmvc.v2;
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,28 +20,24 @@ public class SpringMemberControllerV2 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @RequestMapping("/new-form")
-    public ModelAndView newForm() {
-        return new ModelAndView("new-form");
+    public String newForm() {
+        return "new-form";
     }
 
     @RequestMapping
-    public ModelAndView members() {
+    public String members(Model model) {
         List<Member> members = memberRepository.findAll();
-        ModelAndView mv = new ModelAndView("members");
-        mv.addObject("members", members);
-        return mv;
+        model.addAttribute("members", members);
+        return "members";
     }
 
-    @RequestMapping("/save")
-    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+    @PostMapping("/save")
+    public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model) {
 
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        ModelAndView mv = new ModelAndView("save-result");
-        mv.addObject("member", member);
-        return mv;
+        model.addAttribute("member", member);
+        return "save-result";
     }
 }
